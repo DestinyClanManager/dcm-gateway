157c905cf6197672c6cf98a5b24022e2805238a3
Tom Carter <heymrcarter14@gmail.com>
2018-08-10 21:18:56 -0400

    adds rollbar log when app starts

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import Rollbar from 'rollbar'
 const { PORT = 3000, NODE_ENV, ROLLBAR_ACCESS_TOKEN } = process.env
 
 if (NODE_ENV === 'production') {
-  new Rollbar({
+  const rollbar = new Rollbar({
     accessToken: ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
     captureUnhandledRejections: true
   })
+
+  rollbar.info('dcm-gateway starting...')
 }
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
